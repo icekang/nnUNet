3c02c11610296e4d236d92b94b7de5a1d3b03ff0
icekang <naravich@mit.edu>
2024-04-16 14:46:54 -0400

    feat: remove multiprocessing in data augmentation

--- train_all_configurations.sh
+++ train_all_configurations.sh
@@ -1,21 +1,10 @@
-DATASET_ID=302
+DATASET_ID=300
 
-# for fold in $(seq 0 2); do
-# 	nnUNetv2_train -tr GenesisNNUNetTrainer $DATASET_ID 3d_fullres $fold --val_best
-# done
-
-for fold in $(seq 0 2); do
-	nnUNetv2_train -tr nnUNetTrainerScaleAnalysis2 $DATASET_ID 3d_fullres $fold --val_best
-done
+# export nnUNet_def_n_proc=10
+export nnUNet_n_proc_DA=0
 
 for fold in $(seq 0 2); do
-	nnUNetv2_train -tr nnUNetTrainerScaleAnalysis2 $DATASET_ID 2d $fold --val_best
-done
-
-for fold in $(seq 0 2); do
-	nnUNetv2_train -tr nnUNetTrainerScaleAnalysis4 $DATASET_ID 3d_fullres $fold --val_best
-done
-
-for fold in $(seq 0 2); do
-	nnUNetv2_train -tr nnUNetTrainerScaleAnalysis4 $DATASET_ID 2d $fold --val_best
+	nnUNetv2_train -tr nnUNetTrainerScaleAnalysis4 $DATASET_ID 2d $fold --val_best --c
+    echo "Sleeping for 2 minutes"
+    sleep 2m
 done
