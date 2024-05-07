26887f2e44a7c23cc8c4008d1631cea3c1b4be47
Naravich Chutisilp <naravich@mit.edu>
2024-05-07 14:19:02 -0400

    chore: add SLURM submission scripts and results

---
+++ test_finetuned_with_LaW_to_Calcium_OCT_submission_2d_fold_0_1_and_2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Activating the conda environment
+source activate nnunet
+which python
+
+# Setup env variables nn_UNet
+export nnUNet_raw="/home/gridsan/nchutisilp/datasets/nnUNet_Datasets/nnUNet_raw"
+export nnUNet_preprocessed="/home/gridsan/nchutisilp/datasets/nnUNet_Datasets/nnUNet_preprocessed"
+export nnUNet_results="/home/gridsan/nchutisilp/datasets/nnUNet_Datasets/nnUNet_results"
+
+DATASET_ID=302 # Calcium
+for fold in $(seq 0 2); do
+    for CONFIG in 2d; do
+        for TRAINER in nnUNetTrainer; do
+            echo "FOLD $fold CONFIG $CONFIG TRAINER $TRAINER"
+            INPUT_DIR=$nnUNet_raw/Dataset302_Calcium_OCTv2/imagesTs
+            OUTPUT_DIR=$nnUNet_results/Dataset302_Calcium_OCTv2/${TRAINER}__nnUNetPlans__${CONFIG}/fold_${fold}_finetuned_with_LaW_test
+            LABEL_DIR=$nnUNet_raw/Dataset302_Calcium_OCTv2/labelsTs
+
+            nnUNetv2_predict -i $INPUT_DIR -o $OUTPUT_DIR -f $fold -c $CONFIG -device cuda -d $DATASET_ID -tr $TRAINER -chk checkpoint_best.pth
+            nnUNetv2_evaluate_simple $LABEL_DIR $OUTPUT_DIR -l 1
+            echo "Completed FOLD $fold CONFIG $CONFIG TRAINER $TRAINER"
+        done
+    done
+done
